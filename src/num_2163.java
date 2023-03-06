bf38aa867b1617ff03f83f31461c8ad1326869da
ekthf2013 <ekthf2013@naver.com>
2023-03-07 00:13:33 +0900

    2023/03/6

---
+++ src/num_2163.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class num_2163 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int cho = N*M;
+        int sum = cho - 1;
+        System.out.println(sum);
+    }
+}
